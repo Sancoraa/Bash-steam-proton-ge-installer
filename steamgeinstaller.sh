c25f11f685e0809b7e6048638f8701bf53924fbc
Sancoraa <sancora@orange.fr>
2022-07-10 20:40:41 +0200

    webscraping version's names

--- steamgeinstaller.sh
+++ steamgeinstaller.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 
+curl -s https://github.com/GloriousEggroll/proton-ge-custom/releases > wscraping.tmp
+grep -o "/GE-Proton.*tar.gz" wscraping.tmp > versions.txt
+sed -i  '1d;3d;5d;7d;9d;11d;13d;15d;17d;19d' versions.txt
+sed -ie 's/^.//' versions.txt
+sed -i 's/.tar.gz//' versions.txt
+
+eval $(awk '{print "proton"NR"="$1}' versions.txt)
+
 PS3="Select the Glorious Eggroll's version you wish to install on Steam: "
 declare -A options=(
-[opt1]="GE-Proton7-24"
-[opt2]="GE-Proton7-23"
-[opt3]="GE-Proton7-22"
-[opt4]="GE-Proton7-21"
-[opt5]="GE-Proton7-20"
-[opt6]="GE-Proton7-19"
-[opt7]="GE-Proton7-18"
-[opt8]="GE-Proton7-17"
-[opt9]="GE-Proton7-16"
-[opt10]="GE-Proton7-15"
+[opt1]="$proton1"
+[opt2]="$proton2"
+[opt3]="$proton3"
+[opt4]="$proton4"
+[opt5]="$proton5"
+[opt6]="$proton6"
+[opt7]="$proton7"
+[opt8]="$proton8"
+[opt9]="$proton9"
+[opt10]="$proton10"
 )
 
 select opt in "${options[@]}"
